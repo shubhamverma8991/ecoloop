3ea66a69fbbbc11905fd1d28c26c6c20c5b9a180
ShubhvermaMakerobos <shubham@makerobos.com>
2023-06-16 13:17:31 +0530

    Day 3

--- src/components/contactUs/Contact.js
+++ src/components/contactUs/Contact.js
@@ -2,38 +2,57 @@ import React from "react";
 import "./Contact.css";
 export default function Contact() {
   return (
-    <div className="contact">
-      <div className="innerContainer wd-40">
-        <div>Email</div>
-        <div>Whatsapp</div>
-        <div>facebook</div>
-        <div>Linkedln</div>
-      </div>
-      <div className="innerContainer wd-60">
-        <div>
-          <label htmlFor="name">Name</label>
-          <br />
-          <input type="text" name="name" id="name" />
+    <div className="ContactDiv">
+      <h4 className="sectionhead">Contact US</h4>
+      <div className="contact">
+        <div className="innerContainer wd-40">
+          <div>
+            <h3>Email</h3>
+            <h4>Hi@gmail.com</h4>
+          </div>
+          <div>
+            <h3>WhatsApp</h3>
+            <h4>+91 123,456,789,0</h4>
+          </div>
+          <div>
+            <h3>FaceBook</h3>
+            <h4>https.facebook.com</h4>
+          </div>
+          <div>
+            <h3>Linkedln</h3>
+            <h4>https:linkedln.com</h4>
+          </div>
         </div>
-        <div className="phone">
-          <label htmlFor="contact">Mobile No</label>
+        <div className="innerContainer wd-60">
+          <div>
+            {/* <label htmlFor="name">Name</label>
+          <br /> */}
+            <input type="text" name="name" id="name" placeholder="Enter Name Here" />
+          </div>
+          <div className="phone">
+            {/* <label htmlFor="contact">Mobile No</label>
+          <br /> */}
+            <input type="number" name="contact" id="contact" placeholder="Enter Mobile Number" />
+          </div>
+          <div className="email">
+            {/* <label htmlFor="email">Email Id</label>
+          <br /> */}
+            <input type="email" name="email" id="email" placeholder="Enter Email Id Here" />
+          </div>
+          <div className="company">
+            {/* <label htmlFor="company">Company Name</label>
+          <br /> */}
+            <input type="text" name="company" id="company" placeholder="Enter Company Name" />
+          </div>
+          <div className="enquiry">
+            {/* <label htmlFor="msg">Enquiry</label>
           <br />
-          <input type="number" name="contact" id="contact" />
-        </div>
-        <div className="email">
-          <label htmlFor="email">Email Id</label>
-          <br />
-          <input type="email" name="email" id="email" />
-        </div>
-        <div className="company">
-          <label htmlFor="company">Company Name</label>
-          <br />
-          <input type="text" name="company" id="company" />
-        </div>
-        <div className="enquiry">
-          <label htmlFor="msg">Enquiry</label>
-          <br />
-          <textarea placeholder="Please write your Enquiry" name="msg" id="msg" rows="10"></textarea>
+          <br /> */}
+            <textarea placeholder="Please write your Enquiry" name="msg" id="msg" rows="10"></textarea>
+          </div>
+          <div className="button">
+            <button>Submit a Enquiry</button>
+          </div>
         </div>
       </div>
     </div>
